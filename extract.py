1cf577cf68b6cf3c1d67acbe236832e72d916474
Rupa-16 <90505081+Rupa-16@users.noreply.github.com>
2021-09-13 22:18:57 +0530

    Add files via upload

--- extract.py
+++ extract.py
@@ -1,6 +1,6 @@
 # Your imports go here
 import logging
-
+import json
 logger = logging.getLogger(__name__)
 
 '''
@@ -13,9 +13,47 @@ logger = logging.getLogger(__name__)
     float: returns the extracted amount
 
 '''
+
 def extract_amount(dirpath: str) -> float:
+    jsonpath=dirpath+"\ocr.json"
+    file = open(jsonpath,)
+    data=json.load(file)
+    index=0
+    output=0
+    keywords=["1.00N","AMOUNT PAID","Total :","PAYMENTS","CREDIT","Total","TOTAL","TOTAL AMOUNT","Payment","Total:","Theater and Dance","PAID","Order Total","Payment via Credit Card (usina card"]
+    for i in data['Blocks']:
+        try:
+            text=i['Text']
+            # logger.info("checking %s in ===>%s",i,text)
+            if text in keywords:
 
-    logger.info('extract_amount called for dir %s', dirpath)
+                # logger.info("true %s,%s",text,keywords)
+
+                # if isinstance(data["Blocks"][index+1]["Text"],int):
+                output=data["Blocks"][index+1]["Text"]
+                logger.info("%s %s",text,output)
+                if output == "SPRING ":
+                    continue
+                if output == "USD":
+                    output = data["Blocks"][index + 2]["Text"]
+                if output=="$":
+                    output = data["Blocks"][index + 2]["Text"]
+                output=output.replace("$", "")
+                output = output.replace(",", "")
+                output = output.replace("(", "")
+                output = output.replace(")", "")
+                try:
+                    output=float(output);
+                    logger.info('extract_amount called for dir %s ->%s->%s', dirpath, text,output)
+                    break
+                except ValueError:
+                    print("value error")
+        except KeyError:
+            logger.info("e")
+            # int sample=6
+        index=index+1;
+
     # your logic goes here
-
-    return 0.0
+    # print(dirpath)
+    logger.info("returning %s",output)
+    return output
